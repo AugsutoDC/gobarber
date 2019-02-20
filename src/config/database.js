1512f8040ec6dcb9dd0b3c8691f27714e6e6d2b8
AugsutoDC <dan.correia10@hotmail.com>
2019-02-20 16:40:15 -0300

    GoNode Modulo 2
    GoNode Modulo 2

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'postgres',
+  host: '187.17.94.81',
+  username: 'docker',
+  password: 'docker',
+  databse: 'gonodemodulo2',
+  operatorAliases: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+}
